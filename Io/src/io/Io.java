11d8e9a7b6c95093b5fe065b7d3d4c6e7d24d19f
Dmitriy <kuliniuch@rambler.ru>
2012-06-03 22:57:34 +0400

    io: input using InputStreamReader class

---
+++ Io/src/io/Io.java
@@ -0,0 +1,22 @@
+package io;
+
+import java.io.*;
+
+public class Io {
+
+    public static void main(String[] args) throws Exception {
+        String firstName;
+        String lastName;
+
+        InputStreamReader thread = new InputStreamReader(System.in);
+        BufferedReader keyboard = new BufferedReader(thread);
+
+        System.out.println("What is first your name ?");
+        firstName = keyboard.readLine();
+
+        System.out.println("What is your last name ?");
+        lastName = keyboard.readLine();
+
+        System.out.print("Now I know who you are, you are " + firstName + " " + lastName);
+    }
+}
