1119100a509199b7a377cb3ee0169ff5502cf656
Dmitriy Kulinich <kuliniuch@rambler.ru>
2012-06-19 01:06:24 +0300

    io: use auto-formatting

--- Io/src/io/Io.java
+++ Io/src/io/Io.java
@@ -1,22 +1,23 @@
 package io;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Io {
 
-    public static void main(String[] args) throws Exception {
-        String firstName;
-        String lastName;
+	public static void main(String[] args) throws Exception {
+		String firstName;
+		String lastName;
 
-        InputStreamReader thread = new InputStreamReader(System.in);
-        BufferedReader keyboard = new BufferedReader(thread);
+		InputStreamReader thread = new InputStreamReader(System.in);
+		BufferedReader keyboard = new BufferedReader(thread);
 
-        System.out.println("What is first your name ?");
-        firstName = keyboard.readLine();
+		System.out.println("What is first your name ?");
+		firstName = keyboard.readLine();
 
-        System.out.println("What is your last name ?");
-        lastName = keyboard.readLine();
+		System.out.println("What is your last name ?");
+		lastName = keyboard.readLine();
 
-        System.out.print("Now I know who you are, you are " + firstName + " " + lastName);
-    }
+		System.out.print("Now I know who you are, you are " + firstName + " " + lastName);
+	}
 }
